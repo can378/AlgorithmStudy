b8b46694232cccd34aa6e31503cb16cb60bf7de4
can378 <yunji378mm@naver.com>
2026-03-02 01:00:07 +0900

    1865

--- AlgorithmBJ/bj1865_2.cpp
+++ AlgorithmBJ/bj1865_2.cpp
@@ -2,18 +2,55 @@
 #include <vector>
 
 using namespace std;
+typedef pair<int, int>p;
+vector<vector<p>>graph;
+int n, load, wormhole;
+int MAX = 1000000000;
 
+string bellman() {
 
-void bellman() {
+	vector<int>dist(n + 1,0);
+	for (int iter = 1; iter <= n; iter++) {
+		bool is_update = false;
+		for (int u = 1; u <= n; u++) {
+			for (auto p : graph[u]) {
+				int w = p.first, v = p.second;
+				if (dist[v] > dist[u] + w) {
+					dist[v] = dist[u] + w;
+					is_update = true;
+				}
+			}
+		}
+		if (!is_update) break;
+	}
 
+	for (int u = 1; u <= n; u++) {
+		for (auto p : graph[u]) {
+			int w = p.first, v = p.second;
+			if (dist[v] > dist[u] + w) return "YES";
+		}
+	}
+	return "NO";
 }
 
 void task() {
-	int n, load, w; cin >> n >> load >> w;
-	for (int i = 0; i < load; i++) {
-		int s, e, t; cin >> s >> e >> t;
+	cin >> n >> load >> wormhole;
 
+	graph.assign(n + 1, {});
+	int s, e, t;
+	for (int i = 0; i < load; i++) {
+		cin >> s >> e >> t;
+		graph[s].push_back({ t,e });
+		graph[e].push_back({ t,s });
 	}
+
+	for (int i = 0; i < wormhole; i++) {
+		cin >> s >> e >> t;
+		graph[s].push_back({ -t,e });
+	}
+
+
+	cout<<bellman()<<"\n";
 }
 
 int main() {
